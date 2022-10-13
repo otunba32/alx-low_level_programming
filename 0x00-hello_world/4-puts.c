39a5220abbdf0c7e8270bfc87b1d2c80a5c046fb
otunba32 <emmanuel.abia32@gmail.com>
2022-10-13 09:54:49 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+
+/**
+ * main - prints("Programming is like building a multilingual puzzle")
+ *
+ * Return: successful
+ */
+int main(void)
+{
+	put("/""Programming is like building a multilingual puzzle");
+	return(0);
+}
